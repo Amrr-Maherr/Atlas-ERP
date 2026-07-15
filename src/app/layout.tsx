40f902c08da51ea5d8ff264dbf00dd7f8078fde8
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-07-15 13:42:08 +0300

    feat: add React Query provider with SSR support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Providers } from "@/providers";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
@@ -30,7 +31,9 @@ export default function RootLayout({
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
