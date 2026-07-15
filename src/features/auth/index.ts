ad2e15a6b6dd3df5b8f6b952269aa081f4c8169b
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-07-15 15:22:39 +0300

    fix(auth): add useLogin and handleLogin to barrel exports

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,2 +1,4 @@
 export { LoginForm } from "./components/login-form"
 export { ValidationMessage } from "./components/ValidationMessage"
+export { default as useLogin } from "./hooks/useLogin"
+export { default as handleLogin } from "./api/handleLogin"
