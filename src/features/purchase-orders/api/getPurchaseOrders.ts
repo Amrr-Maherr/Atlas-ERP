d7bd9fd02cf9fa24abe048cce3698e8ac1cbb7f8
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-07-16 11:04:57 +0300

    feat(purchase-orders): add GET API layer and React Query hooks

---
+++ src/features/purchase-orders/api/getPurchaseOrders.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001",
+});
+
+export default async function getPurchaseOrders() {
+    const { data } = await api.get("/purchaseOrders");
+    return data;
+}
