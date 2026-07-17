b0de5fc5cd2acdf0a661425a8982236a5361f4c2
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-07-17 19:20:13 +0300

    feat: export products components from barrel

--- src/features/products/index.ts
+++ src/features/products/index.ts
@@ -1,2 +1,16 @@
 export { getProducts, getProduct } from "./api"
 export { useProducts, useProduct } from "./hooks"
+export { ProductsList } from "./components/products-list";
+export { ProductsTable } from "./components/products-table";
+export { ProductsTableRow } from "./components/products-table-row";
+export { ProductsSkeleton } from "./components/products-skeleton";
+export { ProductsError } from "./components/products-error";
+export { ProductsEmpty } from "./components/products-empty";
+export { ProductStatusBadge } from "./components/product-status-badge";
+export { ProductDetails } from "./components/product-details";
+export { ProductDetailsHeader } from "./components/product-details-header";
+export { ProductDetailsCard } from "./components/product-details-card";
+export { ProductDetailsSkeleton } from "./components/product-details-skeleton";
+export { ProductDetailsError } from "./components/product-details-error";
+export { ProductEditDialog } from "./components/product-edit-dialog";
+export type { Product } from "./types/product.types";
